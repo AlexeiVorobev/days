05771d2fbff9c2e333964236a541ccfc26de4d15
alexthetaffer <alexthetaffer@gmail.com>
2023-03-30 13:54:37 +0300

    Create Login and sign-up pages

---
+++ client/src/app/routes/Register.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  };
+
+  return (
+    <div className="form-container">
+      <section className="heading">
+        <h1>Days</h1>
+        <p>“Write some shit down.”</p>
+      </section>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          placeholder="Email"
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          onChange={onChange}
+        />
+        <button type="submit" className="btn-special">
+          Sign-up
+        </button>
+      </form>
+      <section className="subscript">
+        <p>Already have an account?</p>
+        <Link to="/login">Login</Link>
+      </section>
+    </div>
+  );
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router >
       <App />
     </Router>
   </React.StrictMode>
