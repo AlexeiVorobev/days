3fdbcbab85d1567cda228901b6e5f580112acb85
Alex <alexthetaffer@gmail.com>
2023-04-20 10:52:28 +0300

    Prepare to deploy

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -18,6 +19,15 @@ app.use(cors({
 
 app.use('/api/notes', require('./routes/noteRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+//Serve frontend
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "../", 'client', 'dist', 'index.html')))
+} else {
+  app.get('/', (req, res) => res.send('Please set to'))
+}
 
 app.use(errorHandler);
 
